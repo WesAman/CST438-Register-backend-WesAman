b95f75c95e5d37c3ec38ded32760c46f740cddd1
WesAman <waman@csumb.edu>
2023-10-26 18:15:33 -0700

    Fixed! Woohoo!

---
+++ cst438_registration/src/main/java/com/cst438/domain/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cst438.domain;
+
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository <User, Integer> {
+	
+	 User findByUsername(String email); 
+	
+
+}
+
