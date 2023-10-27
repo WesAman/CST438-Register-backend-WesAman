4a5e8913146b7ca2718eba48e488b18f122e7627
WesAman <waman@csumb.edu>
2023-10-26 18:15:51 -0700

    Fixed, just need to work with front-end aspect

---
+++ cst438_registration/src/main/java/com/cst438/controller/LoginController.java
@@ -0,0 +1,54 @@
+package com.cst438.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cst438.domain.User;
+import com.cst438.domain.UserRepository;
+import com.cst438.dto.AccountCredentials;
+import com.cst438.service.JwtService;
+
+
+@RestController
+public class LoginController {
+	@Autowired
+	private JwtService jwtService;
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired	
+	AuthenticationManager authenticationManager;
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public ResponseEntity<?> getToken(@RequestBody AccountCredentials credentials) {
+		UsernamePasswordAuthenticationToken creds =
+				new UsernamePasswordAuthenticationToken(
+						credentials.username(),
+						credentials.password());
+
+		Authentication auth = authenticationManager.authenticate(creds);
+
+		// Generate token
+		String jwts = jwtService.getToken(auth.getName());
+		// Username from front end--> now we check if stored in sql table
+		User user = userRepository.findByUsername(credentials.username());
+	
+
+		// Build response with the generated token
+		return ResponseEntity.ok()
+				.header(HttpHeaders.AUTHORIZATION, "Bearer " + jwts)
+				.header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization")
+				.body(user.getRole());
+
+	}
+}
